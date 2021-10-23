16569cd26c5122b3c9c22e5b798beb77371df55b
Beytullahzor7 <beytullahzor7@gmail.com>
2021-10-24 01:36:20 +0300

    Day1

---
+++ src/app.js
@@ -0,0 +1,16 @@
+console.log("Merhaba Kodlama.io")
+
+// JS is not typesafe
+let dolarBugun = 9.30
+let dolarDun = 9.20
+
+{
+    let dolarDun = 9.1
+}
+
+console.log(dolarDun)
+
+const euroDun = 11.2
+euroDun = 11.1
+
+console.log(euroDun)
