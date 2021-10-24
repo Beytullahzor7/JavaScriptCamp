affd9779960b2aa79cdfd1b788da238a3099550b
Beytullahzor7 <beytullahzor7@gmail.com>
2021-10-24 15:19:31 +0300

    Day 1 Updated

--- src/app.js
+++ src/app.js
@@ -11,6 +11,18 @@ let dolarDun = 9.20
 console.log(dolarDun)
 
 const euroDun = 11.2
-euroDun = 11.1
+//euroDun = 11.1 hata döndürür çünkü euroDun bir constant değerdir
 
 console.log(euroDun)
+
+let konutKredileri = ["Konut kredisi","Emlak Konut Kredisi","Kamu konut kredisi","Özel konut kredisi","Özel konut kredisi"]
+
+console.log("<ul>")
+
+for (let i = 0; i<konutKredileri.length;i++){ //i nin değeri konutkredilerinden küçük olduğu sürece işleme devam et
+    console.log("<li>"+ konutKredileri[i]+ "</li>")
+}
+console.log("</ul>")
+console.log(konutKredileri.length)
+
+//console.log(konutKredileri)
