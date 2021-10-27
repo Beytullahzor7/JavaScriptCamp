a6ec657a6e015e7f588b0fb9d0c1331297cfb416
Beytullahzor7 <beytullahzor7@gmail.com>
2021-10-27 20:23:07 +0300

    Day1 completed

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ const euroDun = 11.2
 
 console.log(euroDun)
 
-let konutKredileri = ["Konut kredisi","Emlak Konut Kredisi","Kamu konut kredisi","Özel konut kredisi","Özel konut kredisi"]
+let konutKredileri = ["Konut kredisi","Emlak Konut Kredisi","Kamu konut kredisi","Özel konut kredisi"]
 
 console.log("<ul>")
 
@@ -26,3 +26,47 @@ console.log("</ul>")
 console.log(konutKredileri.length)
 
 //console.log(konutKredileri)
+
+let student = {id:1, name: "beytullah"}
+
+function save(ogrenci, puan=80) {
+    console.log(ogrenci.name + ":" + puan)
+}
+save(student);
+save(student,100) //puan göndermezsek default olarak 80 alır 
+//student parametresi ilk olduğu için fonk içerisindeki öğrenciye denk gelir
+
+function save1(puan=10, ogrenci) {
+    console.log(ogrenci.name + ":" + puan)
+}
+save1(undefined,student) //java ve python gibi dillerde default deger sona yazılır ancak js de undefined ekleyerek ilk parametreye default değer ataması yapabiliriz
+
+
+//REST
+let showProducts = function (id,...products) { //değişkene fonksiyon ataması yaptık. buradaki üç nokta gönderilen elemanların tek array olduğunu belirtir
+    console.log(id)
+    console.log(products)
+}
+
+// showProducts(10,"elma","armut","karpuz")
+
+//SPREAD
+let points = [1,2,3,4,50,60,12]
+console.log(...points) //arrayı parcaladık
+console.log(Math.max(...points))           
+console.log(..."ABC","D","E",..."FG")         
+
+//DESTRUCTURING
+
+let populations = [1000,2000,3000]
+let [small,medium,high] = populations //bu bir destructuring örneğidir
+
+console.log(small)
+console.log(medium)
+console.log(high)
+
+function someFunction([small1],number) { //populations arrayı içerisindeki ilk değeri small1 olarak atama yapar
+    console.log(small1)
+}
+
+someFunction(populations)
---
+++ src/oop/index.js
@@ -0,0 +1,35 @@
+class Customer{
+    constructor(id, customerNumber){
+
+        this.id = id //Bu bir prototyping özelliğidir
+        this.customerNumber = customerNumber
+    }
+
+}
+
+let customer = new Customer(1,"1234");
+
+//PROTOTYPING
+customer.name = "Beytullah ZOR" //instanceye yapılan prototyping
+console.log(customer.name)
+
+Customer.something = "Something"
+console.log(Customer.something)
+
+console.log(customer.id, customer.customerNumber)
+
+
+class IndividualCustomer extends Customer{
+    constructor(firstName, id, customerNumber){
+        super(id, customerNumber) //base de olan özellikler
+        this.firstName = firstName
+    }
+}
+
+class CorporateCustomer extends Customer{
+    constructor(companyName, id , customerNumber){
+        super(id, customerNumber)
+        this.companyName = companyName
+    }
+    
+}
